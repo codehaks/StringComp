5bdb6a40064c4c1678ac9c44828477d74fbd925c
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-31 22:36:45 +0330

    Commands

--- Test.App/Program.cs
+++ Test.App/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PeopleApp.Data;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Test.Data;
 
@@ -14,13 +17,52 @@ namespace Test.App
 
             var provider = services.BuildServiceProvider();
 
-            using (var db = provider.GetService<PeopleDbContext>())
+            _db = provider.GetService<PeopleDbContext>();
+            Users = _db.Users.ToList();
+
+            if (args != null)
             {
-                Console.WriteLine(db.Users.Count());
+                switch (args[0])
+                {
+                    case "tolower":
+                        TestToLower();
+                        break;
+
+                    case "equal":
+                        TestEqual();
+                        break;
+
+                    default:
+                        break;
+                }
+
             }
 
+        }
 
-            
+        private static PeopleDbContext _db;
+
+        private static IList<User> Users;
+
+        private static void TestToLower()
+        {
+
+
+            int count = 0;
+
+            var sw = Stopwatch.StartNew();
+            count = Users.Where(u => u.Givenname.ToLower() == "jack").Count();
+            sw.Stop();
+            Console.WriteLine($"Found : {count} => Time : {sw.ElapsedMilliseconds,3:N0}");
+        }
+
+        private static void TestEqual()
+        {
+            int count = 0;
+            var sw = Stopwatch.StartNew();
+            count = Users.Where(u => string.Equals(u.Givenname, "jack", StringComparison.OrdinalIgnoreCase)).Count();
+            sw.Stop();
+            Console.WriteLine($"Found : {count} => Time : {sw.ElapsedMilliseconds,3:N0}");
         }
     }
 }
