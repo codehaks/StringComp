c1bb0849f64d4937102845142f483b315245e5da
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-01 12:24:19 +0330

    ByteComp

---
+++ ByteApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ByteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte byteValue1 = 201;
+            //Console.WriteLine(byteValue1);
+
+            //byte byteValue2 = 0x00C9;
+            //Console.WriteLine(byteValue2);
+
+            
+
+            byte byteValue3 = 0b101010;
+            Console.WriteLine(byteValue3);
+        }
+    }
+}
