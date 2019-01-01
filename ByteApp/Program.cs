8c3adbfc79199e68e27a5a45a8780e06afd51b8a
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-01 20:16:20 +0330

    Bits

--- ByteApp/Program.cs
+++ ByteApp/Program.cs
@@ -16,16 +16,36 @@ namespace ByteApp
 
             var bits1 = new BitArray(Encoding.ASCII.GetBytes("jack"));
             var bits2 = new BitArray(Encoding.ASCII.GetBytes("jack"));
+
+            Console.WriteLine(ToBinary(bits1));
+            Console.WriteLine(ToBinary(bits2));
+            Console.WriteLine(ToBinary(bits1.Xor(bits2)));
+
+
             var bits3 = bits1.And(bits2);
-            ShowBits(bits1,bits3);
+            //ShowBits(bits1,bits3);
 
         }
 
-        private static void ShowBits(BitArray bits1,BitArray bits2)
+        private static string ToBinary(BitArray bits)
+        {
+            //var bytes = Encoding.ASCII.GetBytes(term);
+            //            var bits = new BitArray(bytes);
+            var result = "";
+
+            foreach (var bit in bits)
+            {
+                result = result + Convert.ToByte(bit).ToString();
+            }
+
+            return result;
+        }
+
+        private static void ShowBits(BitArray bits1, BitArray bits2)
         {
             for (int i = 0; i < bits1.Length; i++)
             {
-                Console.WriteLine($" {bits1[i]} - {bits2[i]}");
+                Console.WriteLine($" {Convert.ToByte(bits1[i])} - {Convert.ToByte(bits2[i])}");
             }
 
             Console.WriteLine("--------------");
